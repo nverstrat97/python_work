49762b402dc0bcb357db478d5da2aa619b03ed69
nverstrat97 <nverstrat@gmail.com>
2025-02-28 10:27:20 -0500

    Day 3 - Commit 1 - Python Crash Course

---
+++ crash_course_ch1/jules.py
@@ -0,0 +1,5 @@
+# Author: Nathan VerStrat - greeting for wife - 2/28/2025
+first_name = "Julianna"
+last_name = "VerStrat"
+full_name = f"{first_name} {last_name}"
+print("Hello, " + full_name.title() + "!")
---
+++ crash_course_ch1/python_people_math_quiz.py
@@ -0,0 +1,9 @@
+# say hello to everyone
+print("Hello Python people! \nTry out this math quiz and find the variables...")
+# math quiz
+x, y, z = 2, 3, 5
+print(f"x = {x}")
+print(f"y = {y}")
+print(f"z = x + y")
+# I don't know how to take input yet, so I'll just print the question
+print("what is z?")
