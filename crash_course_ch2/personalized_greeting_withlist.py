55cf96bc8bba6ebc035eac6379f1a9bce50697ba
nverstrat97 <nverstrat@gmail.com>
2025-02-28 10:40:15 -0500

    Day 3 - Commit 2 - Python Crash Course - Personalized Greeting with List

---
+++ crash_course_ch2/personalized_greeting_withlist.py
@@ -0,0 +1,8 @@
+# first attempt at using a list to store names and print a personalized greeting
+names = ['julianna', 'mike', 'candy', 'aidan', 'zoe', 'emmerson']
+# printing first name in list
+messageFirst = f"Hello, {names[0].title()}!"
+print(messageFirst)
+# print last name in list
+messageLast = f"Hello, {names[-1].title()}!"
+print(messageLast)
