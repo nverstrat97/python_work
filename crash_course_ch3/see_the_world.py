bd5a5383536e869c1336b3e14f66d8aa3b685c47
nverstrat97 <nverstrat@gmail.com>
2025-03-02 14:55:11 -0500

    Day 5 - Commit 1 - Python Crash Course

---
+++ crash_course_ch3/see_the_world.py
@@ -0,0 +1,14 @@
+# exercises in sorting lists
+places = ['new zealand', 'norway', 'iceland', 'japan', 'south korea']
+print(places)
+print(sorted(places))
+print(places)
+print(sorted(places, reverse=True))
+print(places)
+places.reverse()
+print(places)
+places.reverse()
+print(places)
+places.sort()
+print(places)
+places.sort(reverse=True)
