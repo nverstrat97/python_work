c90fb99b292cf405caa0dfe8d2b1cc1afbdddf6c
nverstrat97 <nverstrat@gmail.com>
2025-03-03 19:16:33 -0500

    Day 6 - Commit 5 - Python Crash Course

---
+++ crash_course_ch4/tuples.py
@@ -0,0 +1,7 @@
+buffet_tuples = ('rice', 'beans', 'beef', 'peppers', 'chicken')
+print("Original Menu:")
+for food in buffet_tuples:
+    print(food)
+
+buffet_tuples = ('pork', 'corn', 'cilantro', 'onions', 'chicken')
+print("\nNew Menu:")
