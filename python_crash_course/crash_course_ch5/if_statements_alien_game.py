c56751416315a6f18f6b59b92db75a55829020f9
nverstrat97 <nverstrat@gmail.com>
2025-03-05 21:28:27 -0500

    Day 8: 2025-03-05 Nathan VerStrat

---
+++ python_crash_course/crash_course_ch5/if_statements_alien_game.py
@@ -0,0 +1,12 @@
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You got it!\nYou get 10 points!")
+elif alien_color == 'yellow':
+    print("Shoot that Alien!")
+elif alien_color == 'red':
+    print("You missed! Try again!")
+else: 
+    print("Error: Alien not found.")
+
+
