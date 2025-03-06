0dd5ce0dcf483d93f31382f56f02af1bb26d4ddb
nverstrat97 <nverstrat@gmail.com>
2025-03-06 14:36:41 -0500

    Day 9: 2025-03-06 14:37 Nverstrat

---
+++ python_crash_course/crash_course_ch5/favorite_fruit.py
@@ -0,0 +1,11 @@
+favorite_fruit = ['pear', 'raspberry', 'avocado']
+if 'pear' in favorite_fruit:
+   print("You really like pears!")
+if 'raspberry' in favorite_fruit:
+    print("You really like raspberries!")
+if 'avocado' in favorite_fruit:
+    print("You really like avocados!")
+if 'banana' in favorite_fruit:
+    print("You really like bananas!")
+if 'orange' in favorite_fruit:
+    print("You really like oranges!")
