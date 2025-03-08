2b46a1dfbaabf689c9c2d72f0b7d4a007149d5ec
nverstrat97 <nverstrat@gmail.com>
2025-03-08 16:07:18 -0500

    Day 11: 2025-03-08 nverstrat

--- python_crash_course/crash_course_ch5/random_ch5.py
+++ python_crash_course/crash_course_ch5/random_ch5.py
@@ -1,6 +1,15 @@
-print('type "begin" to start the program')
-begin = input()
-if begin == 'begin':
-    print('Type "end" to end the program')
-else:
-    print('You did not type "begin"')
+print('Type "Begin" to start the program')
+input = input().lower()
+if input == 'bbeegin':
+    #this part will tell the user what fruit they like
+    favorite_fruit = ['pear', 'raspberry', 'avocado']
+    if 'pear' in favorite_fruit:
+        print("You really like pears!")
+    if 'raspberry' in favorite_fruit:
+        print("You really like raspberries!")
+    if 'avocado' in favorite_fruit:
+        print("You really like avocados!")
+    if 'banana' in favorite_fruit:
+        print("You really like bananas!")
+    if 'orange' in favorite_fruit:
+        print("You really like oranges!")
