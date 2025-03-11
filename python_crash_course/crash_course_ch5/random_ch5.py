ddbc378442370d25de292881251c7f78dfcdf1d7
nverstrat97 <nverstrat@gmail.com>
2025-03-10 21:59:04 -0400

    Day 13: 2025-03-10 21:59 nverstrat

--- python_crash_course/crash_course_ch5/random_ch5.py
+++ python_crash_course/crash_course_ch5/random_ch5.py
@@ -5,3 +5,6 @@ list_of_players = ['mario', 'luigi', 'peach', 'toad', 'yoshi', 'bowser']
 # random selection
 selected_player = random.choice(list_of_players)
 print(f"The selected player is: {selected_player.title()}")
+
+# List of character moves
+list_of_moves = ['jump', 'run', 'slide', 'duck', 'attack', 'defend']
