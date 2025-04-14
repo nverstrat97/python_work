66b06878b10baeb743b36f33bf50bce8a6a7378c
nverstrat97 <nverstrat@gmail.com>
2025-04-14 12:27:58 -0400

    Day 22: 2025-04-14 12:28 nverstrat

---
+++ python_crash_course/crash_course_ch8/8.1_message.py
@@ -0,0 +1,5 @@
+# my first function program
+def display_message():
+    print("In this chapter, I am learning about functions.")
+# call the function
+display_message()
---
+++ python_crash_course/crash_course_ch8/8.2_favorite_book.py
@@ -0,0 +1,5 @@
+# a function that takes a title as a parameter and prints a message
+def favorite_book(book):
+    print(f"One of my favorite books is {book.title()}.")
+
+favorite_book('The architecture of Happiness by Alain de Botton')
